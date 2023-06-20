e537a9ab0dd629eebd67151ce1445756b98d8701
andrei <andrei@LAPTOP-ETVB3C3H>
2023-06-20 11:33:11 +0200

    Aggiungere i file di progetto.

---
+++ Project_SQLite_for_LifeIn/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPrincipale
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnInsert = New Button()
+        DG1 = New DataGridView()
+        btnShow = New Button()
+        txtGenre = New TextBox()
+        txtPublication = New TextBox()
+        txtISBN = New TextBox()
+        txtTitle = New TextBox()
+        txtID = New TextBox()
+        lblinsert = New Label()
+        lblBookID = New Label()
+        lblTitle = New Label()
+        lbliSBN = New Label()
+        lblGenre = New Label()
+        lblPublication = New Label()
+        txtAuthor = New TextBox()
+        lblAuthor = New Label()
+        CType(DG1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnInsert
+        ' 
+        btnInsert.BackColor = SystemColors.ActiveCaption
+        btnInsert.Font = New Font("Showcard Gothic", 13.8F, FontStyle.Bold, GraphicsUnit.Point)
+        btnInsert.ForeColor = Color.Red
+        btnInsert.Location = New Point(144, 376)
+        btnInsert.Name = "btnInsert"
+        btnInsert.Size = New Size(181, 50)
+        btnInsert.TabIndex = 0
+        btnInsert.Text = "Insert"
+        btnInsert.UseVisualStyleBackColor = False
+        ' 
+        ' DG1
+        ' 
+        DG1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DG1.Location = New Point(447, 25)
+        DG1.Name = "DG1"
+        DG1.RowHeadersWidth = 51
+        DG1.RowTemplate.Height = 29
+        DG1.Size = New Size(966, 334)
+        DG1.TabIndex = 1
+        ' 
+        ' btnShow
+        ' 
+        btnShow.BackColor = SystemColors.HotTrack
+        btnShow.Font = New Font("Showcard Gothic", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        btnShow.ForeColor = Color.Red
+        btnShow.Location = New Point(849, 376)
+        btnShow.Name = "btnShow"
+        btnShow.Size = New Size(181, 50)
+        btnShow.TabIndex = 2
+        btnShow.Text = "Show Books"
+        btnShow.UseVisualStyleBackColor = False
+        ' 
+        ' txtGenre
+        ' 
+        txtGenre.Location = New Point(144, 245)
+        txtGenre.Name = "txtGenre"
+        txtGenre.Size = New Size(181, 27)
+        txtGenre.TabIndex = 4
+        ' 
+        ' txtPublication
+        ' 
+        txtPublication.Location = New Point(144, 310)
+        txtPublication.Name = "txtPublication"
+        txtPublication.Size = New Size(181, 27)
+        txtPublication.TabIndex = 5
+        ' 
+        ' txtISBN
+        ' 
+        txtISBN.Location = New Point(144, 192)
+        txtISBN.Name = "txtISBN"
+        txtISBN.Size = New Size(181, 27)
+        txtISBN.TabIndex = 6
+        ' 
+        ' txtTitle
+        ' 
+        txtTitle.Location = New Point(144, 96)
+        txtTitle.Name = "txtTitle"
+        txtTitle.Size = New Size(181, 27)
+        txtTitle.TabIndex = 7
+        ' 
+        ' txtID
+        ' 
+        txtID.Location = New Point(144, 45)
+        txtID.Name = "txtID"
+        txtID.Size = New Size(181, 27)
+        txtID.TabIndex = 8
+        ' 
+        ' lblinsert
+        ' 
+        lblinsert.AutoSize = True
+        lblinsert.Location = New Point(145, 11)
+        lblinsert.Name = "lblinsert"
+        lblinsert.Size = New Size(86, 20)
+        lblinsert.TabIndex = 9
+        lblinsert.Text = "Insert Book:"
+        ' 
+        ' lblBookID
+        ' 
+        lblBookID.AutoSize = True
+        lblBookID.Location = New Point(80, 48)
+        lblBookID.Name = "lblBookID"
+        lblBookID.Size = New Size(61, 20)
+        lblBookID.TabIndex = 10
+        lblBookID.Text = "BookID:"
+        ' 
+        ' lblTitle
+        ' 
+        lblTitle.AutoSize = True
+        lblTitle.Location = New Point(97, 99)
+        lblTitle.Name = "lblTitle"
+        lblTitle.Size = New Size(41, 20)
+        lblTitle.TabIndex = 11
+        lblTitle.Text = "Title:"
+        ' 
+        ' lbliSBN
+        ' 
+        lbliSBN.AutoSize = True
+        lbliSBN.Location = New Point(94, 195)
+        lbliSBN.Name = "lbliSBN"
+        lbliSBN.Size = New Size(44, 20)
+        lbliSBN.TabIndex = 12
+        lbliSBN.Text = "ISBN:"
+        ' 
+        ' lblGenre
+        ' 
+        lblGenre.AutoSize = True
+        lblGenre.Location = New Point(87, 248)
+        lblGenre.Name = "lblGenre"
+        lblGenre.Size = New Size(51, 20)
+        lblGenre.TabIndex = 13
+        lblGenre.Text = "Genre:"
+        ' 
+        ' lblPublication
+        ' 
+        lblPublication.AutoSize = True
+        lblPublication.Location = New Point(55, 313)
+        lblPublication.Name = "lblPublication"
+        lblPublication.Size = New Size(86, 20)
+        lblPublication.TabIndex = 14
+        lblPublication.Text = "Publication:"
+        ' 
+        ' txtAuthor
+        ' 
+        txtAuthor.Location = New Point(144, 142)
+        txtAuthor.Name = "txtAuthor"
+        txtAuthor.Size = New Size(181, 27)
+        txtAuthor.TabIndex = 15
+        ' 
+        ' lblAuthor
+        ' 
+        lblAuthor.AutoSize = True
+        lblAuthor.Location = New Point(80, 145)
+        lblAuthor.Name = "lblAuthor"
+        lblAuthor.Size = New Size(57, 20)
+        lblAuthor.TabIndex = 16
+        lblAuthor.Text = "Author:"
+        ' 
+        ' frmPrincipale
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1447, 522)
+        Controls.Add(lblAuthor)
+        Controls.Add(txtAuthor)
+        Controls.Add(lblPublication)
+        Controls.Add(lblGenre)
+        Controls.Add(lbliSBN)
+        Controls.Add(lblTitle)
+        Controls.Add(lblBookID)
+        Controls.Add(lblinsert)
+        Controls.Add(txtID)
+        Controls.Add(txtTitle)
+        Controls.Add(txtISBN)
+        Controls.Add(txtPublication)
+        Controls.Add(txtGenre)
+        Controls.Add(btnShow)
+        Controls.Add(DG1)
+        Controls.Add(btnInsert)
+        Name = "frmPrincipale"
+        Text = "BookShop"
+        CType(DG1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnInsert As Button
+    Friend WithEvents DG1 As DataGridView
+    Friend WithEvents btnShow As Button
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents txtPublication As TextBox
+    Friend WithEvents txtISBN As TextBox
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents lblinsert As Label
+    Friend WithEvents lblBookID As Label
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lbliSBN As Label
+    Friend WithEvents lblGenre As Label
+    Friend WithEvents lblPublication As Label
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents lblAuthor As Label
+End Class
---
+++ Project_SQLite_for_LifeIn/Form1.vb
@@ -0,0 +1,54 @@
+﻿
+Imports System.Data.SQLite
+Imports System.IO
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class frmPrincipale
+    Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
+        If txtID.Text = "" Or txtTitle.Text = "" Or txtISBN.Text = "" Or txtPublication.Text = "" Or txtGenre.Text = "" Or txtAuthor.Text = "" Then
+            MessageBox.Show("Fill the Data", "Info")
+        Else
+            Dim Cn As New SQLiteConnection()
+            Cn.ConnectionString = "Data Source=C:\App Visual Basic\Project_SQLite_for_LifeIn\Project_SQLite_for_LifeIn\Dati\Database.db;Version=3;"
+            Dim s As String = "insert into Books (bookID,Title,ISBN,Genre, Publication, Author) Values (@bookID,@Title,@ISBN,@Genre, @Publication, @Author)"
+
+
+            Cn.Open()
+            Dim cmd As New SQLiteCommand(s, Cn)
+            cmd.Parameters.AddWithValue("@bookID", txtID.Text)
+            cmd.Parameters.AddWithValue("@Title", txtTitle.Text)
+            cmd.Parameters.AddWithValue("@ISBN", txtISBN.Text)
+            cmd.Parameters.AddWithValue("@Genre", txtGenre.Text)
+            cmd.Parameters.AddWithValue("@Publication", txtPublication.Text)
+            cmd.Parameters.AddWithValue("@Author", txtAuthor.Text)
+            cmd.ExecuteNonQuery()
+            MessageBox.Show("Successfully Saved ", "Info")
+            Cn.Close()
+            txtID.Text = ""
+            txtTitle.Text = ""
+            txtISBN.Text = ""
+            txtGenre.Text = ""
+            txtPublication.Text = ""
+            txtAuthor.Text = ""
+
+        End If
+    End Sub
+
+    Private Sub btnShow_Click(sender As Object, e As EventArgs) Handles btnShow.Click
+        Dim con As New SQLiteConnection()
+        con.ConnectionString = "Data Source=C:\App Visual Basic\Project_SQLite_for_LifeIn\Project_SQLite_for_LifeIn\Dati\Database.db;Version=3;"
+        Dim s As String = "select * from Books"
+        con.Open()
+        Dim cmd As New SQLiteCommand(s, con)
+        Dim adapter As New SQLiteDataAdapter(s, con)
+        Dim table As New DataTable()
+        table.Clear()
+        adapter.Fill(table)
+        DG1.DataSource = table
+        con.Close()
+
+
+    End Sub
+
+
+End Class
